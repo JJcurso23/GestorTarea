7f80d8f0227d4951e9820e54ce3d68d499783f8b
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-13 13:41:46 +0200

    feat: Actualizado con genericos

--- GestorTarea.cs
+++ GestorTarea.cs
@@ -1,27 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
 
 namespace Tareas
 {
     public class GestorTarea
     {
-        public Dictionary<int, Tarea> Tareas { get; set; }
+        private readonly Dictionary<int, Tarea> _tareas;
         public GestorTarea()
         {
-            Tareas = new Dictionary<int, Tarea>();
+            _tareas = new Dictionary<int, Tarea>();
         }
         public void AgregarTarea(Tarea tarea)
         {
-            if (!Tareas.ContainsKey(tarea.ID)) {
-                Tareas.Add(tarea.ID, tarea);
+            if (tarea == null) throw new ArgumentNullException(nameof(tarea));
+
+            if (!_tareas.ContainsKey(tarea.ID)) {
+                _tareas.Add(tarea.ID, tarea);
+            }
+            else
+            {
+                Console.WriteLine($"Error: Ya existe una tarea con el ID");
             }
         }
         public void EliminarTarea(int id)
         {
-            Tareas.Remove(id);
+            _tareas.Remove(id);
         }
-        public List<Tarea> ObtenerTareas()
+        public IEnumerable<Tarea> ObtenerTareas()
         {
-            return Tareas.Values.ToList();
+            return _tareas.Values.ToList();
+        }
+
+        public Tarea ObtenerPorId(int id)
+        {
+            _tareas.TryGetValue(id, out var tarea);
+            return tarea;
+        }
+
+        public IEnumerable<Tarea> Buscar(Func<Tarea, bool> criterio)
+        {
+            //var resultados = gestor.Buscar(t => t.titulo.Contains("Examen"));
+            //var vencenHoy = gestor.Buscar(t => t.Endday.Date == DateTime.Today);
+            //var pendientes = gestor.Buscar(t => t.Estado == EstadoTarea.Pendiente);
+            return _tareas.Values.Where(criterio);
         }
 
     }
