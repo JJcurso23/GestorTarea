c82b56fa21bd2fc84e14735ca7678cf157110fa5
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-16 12:50:58 +0200

    task: Creacion clase Usuario

---
+++ Usuario.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tareas
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Email { get; set; }
+        public int Edad { get; set; }
+        public bool Activo { get; set; }
+    }
+}
+
