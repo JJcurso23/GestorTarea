998565048395f2ab38684d69e473678ab449a7aa
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-27 13:28:35 +0200

    feat: Crear enum TipoTarea

---
+++ Domain/Enums/TipoTarea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorTarea.Domain.Enums
+{
+    enum TipoTarea
+    {
+        Simple,
+        Pomodoro,
+        Urgente,
+        Recurrente,
+        ConTarea
+    }
+}
