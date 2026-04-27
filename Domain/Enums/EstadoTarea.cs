8e4064be6e83335dd8d8ac90e88a36c4f89d5e1b
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-27 12:06:53 +0200

    feat: migracion de enumerado sobre estado de tarea

---
+++ Domain/Enums/EstadoTarea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorTarea.Domain.Enums
+{       public enum EstadoTarea
+        {
+            Pendiente,
+            EnProgreso,
+            Completada,
+            Cancelada,
+            Vencida
+        }
+}
