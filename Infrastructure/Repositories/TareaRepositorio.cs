1f90341ff87fb605078e89d951c718708e63f848
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-27 12:04:08 +0200

    feat: Crear script Repositorio.cs para gestion de json

---
+++ Infrastructure/Repositories/TareaRepositorio.cs
@@ -0,0 +1,27 @@
+﻿using GestorTarea.Application.DTOs;
+using GestorTarea.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace GestorTarea.Infrastructure.Repositories
+{
+    public class TareaRepositorio: ITareaRepositorio
+    {
+        private readonly string _ruta = "tareas.json";
+
+        public List<TareaDTo> ObtenerTodas()
+        {
+            if (!File.Exists(_ruta)) return new List<TareaDTo>();
+            string json = File.ReadAllText(_ruta);
+            return JsonSerializer.Deserialize<List<TareaDTo>>(json);
+        }
+
+        public void Guardar(List<TareaDTo> tareas)
+        {
+            string json = JsonSerializer.Serialize(tareas);
+            File.WriteAllText(_ruta, json);
+        }
+    }
+}
