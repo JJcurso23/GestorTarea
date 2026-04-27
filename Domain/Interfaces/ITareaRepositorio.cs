a43ec19abfd83ed9b18d439fa09c7b9b5300e9d8
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-27 11:28:13 +0200

    feat: Crear interface para repositorio

---
+++ Domain/Interfaces/ITareaRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using GestorTarea.Application.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GestorTarea.Domain.Interfaces
+{
+    public interface ITareaRepositorio
+    {
+        public List<TareaDTo> ObtenerTodas();
+        public void Guardar(List<TareaDTo> tareas);
+    }
+}
