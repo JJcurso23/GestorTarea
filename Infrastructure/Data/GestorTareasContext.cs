c360d42b5d3903adab91231c7a48d5c5ed07d67b
JJcurso23 <Jhydellcurso@gmail.com>
2026-04-29 13:15:17 +0200

    feat: Crear script de context

---
+++ Infrastructure/Data/GestorTareasContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GestorTarea.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorTarea.Infrastructure.Data
+{
+    public class GestorTareasContext : DbContext
+    {
+        // Cada DbSet representa una tabla en la BD
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Tarea> Tareas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // Indicar a EF Core qué proveedor usar y cómo conectarse
+            options.UseSqlServer(
+                @"Server=localhost\MSSQLLocalDB;" +
+                 "Database=GestorTareas;" +
+                 "Trusted_Connection=True;" +
+                 "TrustServerCertificate=True;"
+            );
+        }
+    }
+}
+
