d5b04d50645a3852c655f394c59bd8fcef220e47
JJcurso23 <Jhydellcurso@gmail.com>
2026-05-06 13:43:49 +0200

    db: generar migracion para sincronizar tablas TPT

---
+++ Migrations/20260506113349_SincronizarColumnas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestorTarea.Migrations
+{
+    /// <inheritdoc />
+    public partial class SincronizarColumnas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
