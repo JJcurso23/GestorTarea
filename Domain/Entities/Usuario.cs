b59eaf15d0827af5db8dce4d49fb5956ec59b150
JJcurso23 <Jhydellcurso@gmail.com>
2026-05-13 13:15:36 +0200

    feat: Añadir PasswordHash a Usuario y generar migracion

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -10,14 +10,15 @@ namespace GestorTarea.Domain.Entities
         public required string Email { get; set; }
         public int Edad { get; set; }
         public bool Activo { get; set; }
-
+        public string PasswordHash { get; set; }
         protected Usuario() { }
 
         [SetsRequiredMembers]
-        public Usuario(string nombre, string email, int edad, bool activo) {
+        public Usuario(string nombre, string email, int edad,string passwordhash, bool activo) {
             this.Nombre = nombre;
             this.Email = email;
             this.Edad = edad;
+            this.PasswordHash = passwordhash;
             this.Activo = activo;
         }
     }
