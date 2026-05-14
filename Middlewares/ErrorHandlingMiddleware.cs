eb4315e8dac8931fe1e39624ff4ce436ca403c61
JJcurso23 <Jhydellcurso@gmail.com>
2026-05-14 13:22:33 +0200

    feat: implementar moddlewares

---
+++ Middlewares/ErrorHandlingMiddleware.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace GestorTarea.Middlewares
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                Error = "Ocurrió un error interno en el servidor.",
+                Detalle = exception.Message
+            };
+
+            var jsonResponse = JsonSerializer.Serialize(response);
+            return context.Response.WriteAsync(jsonResponse);
+        }
+    }
+
+}
